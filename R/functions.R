52b94511efc85abbc6c4ee8b81421b1c5896d7db
Kristina Matthiesen Hansen <krmaha@ph.au.dk>
2026-05-06 10:13:52 +0200

    Working with the map function reading in more that one files at onece

--- R/functions.R
+++ R/functions.R
@@ -5,7 +5,7 @@
 #'
 #' @returns outputs a data frame/tibble
 #'
-read <- function(file_path, max_rows = 100) {
+read <- function(file_path, max_rows = 10) {
   data <- file_path |>
     readr::read_csv(
       show_col_types = FALSE,
